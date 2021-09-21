ef31a55ca5efd06dd5fa8dc58d9d864faf4e3514
Howard Hsien <howard_hsien@cino.com.tw>
2021-09-22 00:45:38 +0800

    add createWD_list function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import datetime
+import csv
 
 
 #=================== Object docotr ============================
@@ -30,12 +31,13 @@ class Doctor :
 
 #=================== Object WorkingDay ========================
 class WorkingDay :
-	def __init__(self, dayOfWeek):
-		if( dayOfWeek > 4): #Monday is 0
+	def __init__(self, date):
+		if( date.weekday() > 4): #Monday is 0
 			self.count = 2;
 		else:
 			self.count = 1;
 		self.arr = np.zeros(5) # The array is for jobs scheduling => needs to filled with dr's id
+		self.date = date
 				
 		
 #==============================================================
@@ -51,13 +53,29 @@ def parseCSV(filename):
 	delta = d2 - d1
 	for i in range(delta.days + 1):
 		d_tmp = d1 + datetime.timedelta(days=i)
-		workingDay_tmp = WorkingDay(d_tmp.weekday())
+		workingDay_tmp = WorkingDay(d_tmp)
 		print("date: %s, weekday: %d, count %d, jobs %s" % (d_tmp, d_tmp.weekday(), workingDay_tmp.count, workingDay_tmp.arr))
+
+# create the working day list
+def createWDList(year, month):
+	ndays = (datetime.date(year, month+1, 1) - datetime.date(year, month, 1)).days
+	d1 = datetime.date(year, month, 1)
+	d2 = datetime.date(year, month, ndays)
+	delta = d2 - d1
+
+	print("year=%d, month=%d, ndays=%d" %(year, month, ndays))
+	for i in range(ndays):
+		workingDay_list.append(WorkingDay(d1+datetime.timedelta(days=i)))
+	for item in workingDay_list:
+		print ("item.date :%s, item.count:%d" %(item.date, item.count))
+	
+#print(workingDay_list)
+
 
 def main():
 #	dr = Doctor(3);
-	td = WorkingDay(datetime.date.today().weekday());
-	wend = WorkingDay(datetime.datetime(2021,9,19).weekday());
+	td = WorkingDay(datetime.date.today());
+	wend = WorkingDay(datetime.datetime(2021,9,19));
 #	print (dr.get_point());
 	print (datetime.date.today().weekday());
 	print (datetime.datetime(2021,9,20).weekday());
@@ -65,6 +83,7 @@ def main():
 	print ("2021.9.19 has %d count" % wend.count)
 	print (td.arr)
 	parseCSV(None)
+	createWDList(2021,9)
 
 # main function starts here
 main();
