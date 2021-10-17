b83175e55d3ebd5913eebfdf273b173f06c40f20
howard hsien <howard_hsien@cino.com.tw>
2021-10-17 23:07:30 +0800

    add support for special rule
    * added readme
    * added support for special rule
    * changed the calendar

--- main.py
+++ main.py
@@ -12,17 +12,23 @@ import csv
 #			5 - r4
 # 			6 - r5
 #			7 - r6
+
+# =========== now don't use the dr-level ==> change to use remaining points =====
 class Doctor :
-	def __init__(self, dr_level, dr_id):
-		self.point = 8 - dr_level;
-		self.id = dr_id;
+	def __init__(self, dr_id, point, special_rule):
+		self.point = point
+		self.id = dr_id
+
+#special rule is for doctors only allowed in some areas		
+		self.special_rule = special_rule
+
 
 #	point = 5;
 	def get_point(self):
-		return self.point;
+		return self.point
 
 	def get_id(self):
-		return self.id;
+		return self.id
 
 
 
@@ -33,10 +39,11 @@ class Doctor :
 class WorkingDay :
 	def __init__(self, date):
 		if( date.weekday() > 4): #Monday is 0
-			self.count = 2;
+			self.count = 2
 		else:
-			self.count = 1;
-		self.jobs = ['']*5#np.zeros(5) # The array is for jobs scheduling => needs to filled with dr's id
+			self.count = 1
+		self.jobs = ['']*6#np.zeros(5) # The array is for jobs scheduling => needs to filled with dr's id
+		self.wards = ['A','B','C','D','E','I']
 		self.date = date
 				
 		
@@ -49,11 +56,18 @@ def parseCSV(filename):
 	# here to read the file and do the parsing
 	with open(filename, newline='') as csvfile:
 		rows = list(csv.reader(csvfile))
-		drDict = dict(zip(rows[1], rows[0]))
-		print(drDict)
-		for dr_id, dr_level in drDict.items():
-			print(dr_id, dr_level)
-			dr_list.append(Doctor(int(dr_level),dr_id))
+		print(rows[2])
+		
+
+		#drDict = dict(zip(rows[0], rows[1])
+		_dr_list = zip(rows[0], rows[1], rows[2])
+		#print(drDict)
+		#for dr_id, remaining_pts in drDict.items():
+		for dr_id, remaining_pts, s_rule in _dr_list:
+			ward_arr = s_rule.split(',')
+			print(dr_id, remaining_pts, ward_arr)
+
+			dr_list.append(Doctor(dr_id, int(remaining_pts), ward_arr))
 		#print(dr_list)
 #	dr_list.append(Doctor()) # Todo: fill the dr info
 #	d1 = datetime.date(2021, 9, 1);
@@ -84,12 +98,31 @@ def findDrWithHighestPoint(dr_list):
 	id_of_the_dr = 0
 #	candidateDr
 	for dr in dr_list:
-		if(dr.get_point() > highestPoint):
+		if(dr.get_point() > 0 and dr.get_point() > highestPoint):
 #			candidateDr = dr
 			highestPoint = dr.get_point()
 			id_of_the_dr = dr.get_id()
 	return id_of_the_dr
-	
+
+def fitRule(dr, ward):
+	for ok_ward in dr.special_rule:
+		if ok_ward == ward:
+			return ward
+
+	return None
+def findDrWithHighestPoint_fitRule(dr_list, ward):
+	highestPoint = 0
+	id_of_the_dr = 0
+#	candidateDr
+	for dr in dr_list:
+		ok_ward = fitRule(dr, ward)
+		if(dr.get_point()> 0 and dr.get_point() > highestPoint and ok_ward): # TODO: arrange guys with higher points
+#		if(ok_ward):			
+#			candidateDr = dr
+			highestPoint = dr.get_point()
+			id_of_the_dr = dr.get_id()
+	return id_of_the_dr
+
 def getDrFromList(dr_id):
 	for dr in dr_list:
 		if(dr.get_id() == dr_id):
@@ -99,10 +132,18 @@ def getDrFromList(dr_id):
 def fillWorkingDay(workingDay):
 	for i in range(len(workingDay.jobs)):
 #	for job in workingDay.jobs:
-		_id = findDrWithHighestPoint(dr_list)
-		getDrFromList(_id).point -= workingDay.count
-		workingDay.jobs[i] = _id
-		print(_id)
+#		_id = findDrWithHighestPoint(dr_list)
+		_id = findDrWithHighestPoint_fitRule(dr_list, workingDay.wards[i])
+		print("id=%s, ward=%s" %(_id, workingDay.wards[i]) )
+		if(_id != 0):
+			getDrFromList(_id).point -= workingDay.count
+			workingDay.jobs[i] = _id
+			print(_id)
+	for i in range(len(workingDay.jobs)):
+		if(workingDay.jobs[i] == ''):
+			_id = findDrWithHighestPoint(dr_list)
+			getDrFromList(_id).point -= workingDay.count
+			workingDay.jobs[i] = _id
 
 def schedule(dr_list, wd_list):
 	for wd in wd_list:
@@ -131,15 +172,17 @@ def createCSVoutput(filePos):
 			for dr in dr_list:
 				_id = dr.get_id()
 				if _id == wd.jobs[0]:
-					date_row.append('A')
+					date_row.append(wd.wards[0])
 				elif _id == wd.jobs[1]:
-					date_row.append('B')
+					date_row.append(wd.wards[1])
 				elif _id == wd.jobs[2]:
-					date_row.append('C')
+					date_row.append(wd.wards[2])
 				elif _id == wd.jobs[3]:
-					date_row.append('D')
+					date_row.append(wd.wards[3])
 				elif _id == wd.jobs[4]:
-					date_row.append('E')
+					date_row.append(wd.wards[4])
+				elif _id == wd.jobs[5]:
+					date_row.append(wd.wards[5])	
 				else:
 					date_row.append('')
 			writer.writerow(date_row)
@@ -154,22 +197,22 @@ def createCSVoutput(filePos):
 
 def main():
 #	dr = Doctor(3);
-	td = WorkingDay(datetime.date.today());
-	wend = WorkingDay(datetime.datetime(2021,9,19));
+#	td = WorkingDay(datetime.date.today())
+#	wend = WorkingDay(datetime.datetime(2021,9,19))
 #	print (dr.get_point());
-	print (datetime.date.today().weekday());
-	print (datetime.datetime(2021,9,20).weekday());
-	print ("today has %d count" % td.count)
-	print ("2021.9.19 has %d count" % wend.count)
-	parseCSV("data/dr-level.csv")
-	createWDList(2021,9)
+#	print (datetime.date.today().weekday())
+#	print (datetime.datetime(2021,9,20).weekday())
+#	print ("today has %d count" % td.count)
+#	print ("2021.9.19 has %d count" % wend.count)
+	parseCSV("data/dr-data-nov.csv")
+	createWDList(2021,11)
 	for doctor in dr_list:
 		print(doctor.get_id(), doctor.get_point())
 	schedule(dr_list, workingDay_list)
 	showDrList()
-	createCSVoutput("output/test.csv")
+	createCSVoutput("output/nov-output.csv")
 
 # main function starts here
-main();
+main()
 
 
